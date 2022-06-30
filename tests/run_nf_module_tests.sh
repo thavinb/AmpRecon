118771c7a68436867856cf9079649c8717d8c17b
Simon Suddaby <ss52@sanger.ac.uk>
2022-06-30 15:27:39 +0100

    specify container path in run

--- tests/run_nf_module_tests.sh
+++ tests/run_nf_module_tests.sh
@@ -4,4 +4,4 @@
 set -e
 
 
-nextflow run tests/test_bamadapter_find.nf -c ampseq-NF-pipeline/nextflow.config -profile standard 
+nextflow run tests/test_bamadapter_find.nf -c ampseq-NF-pipeline/nextflow.config -with-singularity containers/biobambam2.sif 
