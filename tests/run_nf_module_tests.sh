59fd81e5a410abb6f0ff0ef71f8ab743b4c1a04d
Simon Suddaby <ss52@sanger.ac.uk>
2022-06-30 16:13:38 +0100

    change run command

--- tests/run_nf_module_tests.sh
+++ tests/run_nf_module_tests.sh
@@ -4,4 +4,4 @@
 set -e
 
 
-nextflow run tests/test_bamadapter_find.nf -c ampseq-NF-pipeline/nextflow.config -with-singularity containers/biobambam2.sif 
+nextflow run tests/test_bamadapter_find.nf -c ampseq-NF-pipeline/nextflow.config -profile standard 
