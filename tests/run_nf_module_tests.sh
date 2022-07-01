bf23277fe562f3760178a22e0c3caf543b8f7d2d
Simon Suddaby <ss52@sanger.ac.uk>
2022-07-01 14:28:20 +0100

    samtools bam 2 cram test

--- tests/run_nf_module_tests.sh
+++ tests/run_nf_module_tests.sh
@@ -4,4 +4,6 @@
 set -e
 
 
-nextflow run tests/test_bamadapter_find.nf -c tests/test_bamadapter_find.config -profile standard 
+nextflow run test_bamadapter_find.nf -c test_bamadapter_find.config -profile standard 
+wait
+nextflow run test_samtools_bam2cram.nf -c test_samtools_bam2cram.config -profile standard   
