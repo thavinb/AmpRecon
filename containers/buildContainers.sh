1b8164ae408ae798b6ea22998b5c91aee7436b8d
Antonio Marinho <ad45@sanger.ac.uk>
2022-07-05 14:29:53 +0100

    merge current containers branch to develop

--- containers/buildContainers.sh
+++ containers/buildContainers.sh
@@ -23,6 +23,9 @@ singularity build --fakeroot biobambam2.sif SingularityFile_biobambam2
 echo "@ building staden.sif"
 singularity build --fakeroot staden.sif SingularityFile_staden
 
+echo "@ building mergeheaders.sif"
+singularity build --fakeroot mergeheaders.sif SingularityFile_mergeHeaders
+
 echo "@ pulling samtools_1.15.sif"
 singularity pull docker://staphb/samtools:1.15
 
