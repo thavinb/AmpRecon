effa661e781ccc0d7a817cad373d2ccf64d065d5
Simon Suddaby <ss52@sanger.ac.uk>
2022-10-03 11:34:42 +0100

    cd into tests to circumvent config hierarchy

--- tests/run_bwa_alignment.sh
+++ tests/run_bwa_alignment.sh
@@ -2,5 +2,5 @@
 
 
 set -e 
-
-nextflow run tests/test_align_bam.nf -c tests/test_align_bam.config -profile standard  
+cd tests
+nextflow run test_align_bam.nf -c test_align_bam.config -profile standard  
