fb83f26847b33a5cc561c6a8df762e14c9f79fc9
Simon Suddaby <ss52@sanger.ac.uk>
2022-10-04 13:41:11 +0100

    Update run_ci_python_test.sh to have a lower coverage threshold

--- tests/run_ci_python_test.sh
+++ tests/run_ci_python_test.sh
@@ -2,5 +2,5 @@
 
 coverage run --source './bin' -m pytest -v /ampseq-pipeline/tests/python-tests/count_reads_per_region/test_count_reads_per_region.py \
 && coverage html -d ./htmlcov \
-&& coverage report --fail-under 85
+&& coverage report --fail-under 80
 
