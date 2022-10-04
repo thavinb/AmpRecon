3aac5f85af5c6f493010f19f7ed3afe06a9a68f3
Malaria-related pipelines <malpipe@sanger.ac.uk>
2022-10-04 12:08:47 +0100

    add bin module for tests

--- tests/run_ci_python_test.sh
+++ tests/run_ci_python_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-coverage run --source '../bin' -m pytest -v python_tests/count_reads_per_region/test_count_reads_per_region.py \
+coverage run --source './bin' -m pytest -v /ampseq-pipeline/tests/python-tests/count_reads_per_region/test_count_reads_per_region.py \
 && coverage html -d ./htmlcov \
 && coverage report --fail-under 85
 
