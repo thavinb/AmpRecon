6fbee15ce72322fc158c7ce55df5ccdcb7932d2e
Malaria-related pipelines <malpipe@sanger.ac.uk>
2022-10-04 11:45:15 +0100

    update location of test in script

--- tests/run_ci_python_test.sh
+++ tests/run_ci_python_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-coverage run --source './bin' -m pytest -v python_tests/count_reads_per_region/test_*.py \
+coverage run --source '../bin' -m pytest -v python_tests/count_reads_per_region/test_count_reads_per_region.py \
 && coverage html -d ./htmlcov \
 && coverage report --fail-under 85
 
