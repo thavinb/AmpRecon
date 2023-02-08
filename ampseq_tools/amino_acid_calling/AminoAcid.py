ce9ebd8276caa428dcf8f8547832efaaf1acf9ba
Simon Suddaby <ss52@sanger.ac.uk>
2023-02-08 13:51:24 +0000

    Update AminoAcid.py

--- ampseq_tools/amino_acid_calling/AminoAcid.py
+++ ampseq_tools/amino_acid_calling/AminoAcid.py
@@ -1,11 +1,13 @@
 import csv
 import os
+from codon_table import *
+import json
 
 class AminoAcidCaller:
 
-	def __init__(self, sample_id, genotypes_file):
-		#with open(config_file) as conf:
-		#	self.config_file = conf
+	def __init__(self, sample_id, genotypes_file, config_file):
+		with open(config_file) as conf:
+			self.config = json.load(conf)
 		self.sample_id = sample_id
 		self.drl_info = self._read_drl_info()
 		self.codon_key = self._read_codon_key()
@@ -53,7 +55,6 @@ class AminoAcidCaller:
 
 			genotypes['key'][sample_id][chr][row['Loc']] = call
 			genotypes['depth'][sample_id][chr][row['Loc']] = int(row['Depth'])
-
 		return genotypes
 
 
@@ -83,7 +84,6 @@ class AminoAcidCaller:
 		
 		return ret
 
-
 	def _read_codon_key(self):
 		filename = "./amplicon_data/codonKey.txt"
 		if not os.path.isfile(filename):
@@ -112,8 +112,6 @@ class AminoAcidCaller:
 
 		for gene_amino in drl['exp_order']:
 
-			double_het = false
-
 			gene, amino = gene_amino.split(":")
 
 			if gene not in self.haplotypes:
@@ -137,8 +135,7 @@ class AminoAcidCaller:
 				#this is horrible, change it
 				if gene_amino in core_genes:
 					self.haplotypes[gene] += '-'
-				self.haplotypes[gene_amino] = '-'
-				
+				self.haplotypes[gene_amino] = '-'	
 				continue
 			
 			if drl['cons'][chromosome][position_1] != '-':
@@ -153,81 +150,74 @@ class AminoAcidCaller:
 			if base_2 == '-':
 				base_2 = genotype_information['key'][sample_id][chromosome][position_2]
 			if base_3 == '-':
-				base_3 == genotype_information['key'][sample_id][chromosome][position_3]
-
+				base_3 = genotype_information['key'][sample_id][chromosome][position_3]
+		
 			if drl['strand'][gene][amino] == '-':
 				base_1 = self._complement(base_1)
 				base_2 = self._complement(base_2)
 				base_3 = self._complement(base_3)
 
-			if self.is_missing(base_1) or self.is_missing(base_2) or self.is_missing(base_3):
-				#this can go at some point
+			#concatenate
+			codon = f"{base_1}{base_2}{base_3}"
+	
+			if '-' in codon:
 				self.haplotypes[gene_amino] = '-'
 				if gene_amino in core_genes:
 					self.haplotypes[gene] += '-'
 				continue
 
-			if len(base_1) == 1 and len(base_2) == 1 and len(base_3) == 1:
-				aa = self.translate_codon(base_1, base_2, base_3)
+			if len(codon) == 3:
+				aa = self.translate_codon(codon, is_het=False)
 				if gene_amino in core_genes:
 					self.haplotypes[gene] += aa
 				self.haplotypes[gene_amino] = aa
 				continue
 
-			aa_1 = '-'
-			aa_2 = '-'
-			
-			if len(base_1) > 1:
-				allel_1 = list(base_1)[0]
-				allel_2 = list(base_1)[1]
-				aa_1 = self.translate_codon(allel_1, base_2, base_3)
-				aa_2 = self.translate_codon(allel_2, base_2, base_3)
-			elif len(base_2) > 1:
-				allel_1 = list(base_2)[0]
-				allel_2 = list(base_2)[1]
-				aa_1 = self.translate_codon(base_1, allel_1, base_3)
-				aa_2 = self.translate_codon(base_1, allel_2, base_3)
-			elif len(base_3) > 1:
-				allel_1 = list(base_3)[0]
-				allel_2 = list(base_3)[1]
-				aa_1 = self.translate_codon(base_1, base_2, allel_1)
-				aa_2 = self.translate_codon(base_1, base_2, allel_2)
+			elif len(codon) == 4:
+				codon = f"{base_1},{base_2},{base_3}"
+				het_call = self.translate_codon(codon, is_het=True)
 
-			else:
-				#do some double heterozygous logic here
-				double_het = true
-				bases = [base_1, base_2, base_3]
+				if gene_amino in core_genes:
+					self.haplotypes[gene] += het_call
+				self.haplotypes[gene_amino] = het_call
+				continue
 
-				for base in bases:
-					if len(base) > 1:
-						allel_1 = list(base)[0]
-						allel_2 = list(base)[1]
-						
+			if codon in self.config['run_options']['double_heterozygous_cases'][gene_amino]:
+				double_het_call = self.config['run_options']['double_heterozygous_cases'][gene_amino][codon]
+				if gene_amino in core_genes:
+					self.haplotypes[gene] += double_het_call
+				self.haplotypes[gene_amino] = double_het_call
 
-
-			if aa_1 != '-' and aa_2 != '-':
-				if aa_1 == aa_2:
-					aa_call = aa_1
-				else:
-					aa_call = f"[{aa_1}/{aa_2}]"
-
-			self.haplotypes[gene_amino] = aa_call
-
-			if gene_amino in core_genes:
-				#when i get a chance move this code to the start of the loop so i don't re write it all the time
-				self.haplotypes[gene] += aa_call
-
-			if 'PfCRT' in self.haplotypes and self.haplotypes['PfCRT'] == '-V---':
-				self.haplotypes['PfCRT'] == '-----' 
-
-	
+		print(self.haplotypes)
 		return self.haplotypes
 
-	def translate_codon(self, base_1, base_2, base_3):
+	def translate_codon(self, codon, is_het):
 		amino_acid = '-'
-		codon_translations = self.codon_key
-		amino_acid = codon_translations[base_1][base_2][base_3]
-		return amino_acid
+		codon_translations = DNA_Codons
+
+		if is_het == False:
+			amino_acid = codon_translations[codon]
+			return amino_acid
+		else:
+			het_bases = codon.split(",")
+			codon_1 = []
+			codon_2 = []
+			for base in het_bases:
+				if len(base) > 1:
+					codon_1.append(list(base)[0])
+					codon_2.append(list(base)[1])
+				else:
+					codon_1.append(base)
+					codon_2.append(base)
+			
+			codon_1 = ''.join(codon_1)
+			codon_2 = ''.join(codon_2)
+
+			aa_1 = codon_translations[codon_1]
+			aa_2 = codon_translations[codon_2]
+
+			return f"[{aa_1}/{aa_2}]"
+
 
 	def _complement(self, sequence):
 		complement_sequence = []
@@ -236,3 +226,4 @@ class AminoAcidCaller:
 			if nucleotide in self.complement_bases:
 				complement_sequence.append(self.complement_bases[nucleotide])
 		return ''.join(complement_sequence)
+
