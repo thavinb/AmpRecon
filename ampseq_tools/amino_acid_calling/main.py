879e600c8f674196d1b289ff704676dae89c9e26
Simon Suddaby <ss52@sanger.ac.uk>
2023-02-14 14:47:02 +0000

    Update main.py

--- ampseq_tools/amino_acid_calling/main.py
+++ ampseq_tools/amino_acid_calling/main.py
@@ -4,10 +4,14 @@ def main():
 
 	#rudimentary implementation to run methods only
 	
-	a = AminoAcidCaller("test_sample", "./genotype_file.tsv")
-	a.call_haplotypes()
 
+
+
+	a = AminoAcidCaller("SAMPLE_ID", "./genotype_file.tsv", './config.json')
+	a.call_haplotypes()
 
 
 if __name__ == "__main__":
 	main()
+
+	
