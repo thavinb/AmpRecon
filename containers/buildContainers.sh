1ddc528d3b9b5bcd64da83f4f9963d58880d02e8
Malaria-related pipelines <malpipe@sanger.ac.uk>
2023-04-24 16:34:50 +0100

    Added commands for usage building default container

--- containers/buildContainers.sh
+++ containers/buildContainers.sh
@@ -14,6 +14,9 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
+echo "@ building default.sif"
+singularity build --fakeroot default.sif SingularityFile_default
+
 echo "@ building genotyping.sif"
 singularity build --fakeroot genotyping.sif SingularityFile_genotyping
 
