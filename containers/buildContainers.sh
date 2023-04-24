9aea90b3c4f393f3b5b2a9712dd6a598dc9fb266
Malaria-related pipelines <malpipe@sanger.ac.uk>
2023-04-24 16:06:01 +0100

    Readded GATK genotyping containers

--- containers/buildContainers.sh
+++ containers/buildContainers.sh
@@ -14,9 +14,6 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-echo "@ building picard.sif"
-singularity build --fakeroot picard.sif SingularityFile_picard
-
 echo "@ building genotyping.sif"
 singularity build --fakeroot genotyping.sif SingularityFile_genotyping
 
