a1f6a2de8fadb908db65bde02d999e658b49abfd
Tom Maddison <tm17@sanger.ac.uk>
2023-09-18 09:44:47 +0100

    Removed old unit tests, testing modules and test data

--- tests/run_ci_python_test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-coverage run --source './bin' -m pytest -v /ampseq-pipeline/tests/python-tests/count_reads_per_region/test_count_reads_per_region.py \
-&& coverage html -d ./htmlcov \
-&& coverage report --fail-under 80
-
