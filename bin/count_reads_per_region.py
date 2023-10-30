a4d0b2f80010d671d1b93c232d1e92480664a0fe
AMarinhoSN <amarinhosn@pm.me>
2023-10-30 11:25:00 +0000

    revert read counts col names to original

--- bin/count_reads_per_region.py
+++ bin/count_reads_per_region.py
@@ -71,7 +71,7 @@ class QC:
         self._pool = ThreadPool(processes=5)
         self._out_handle = None
         self._output_hdr = (
-            "Rpt,Ref_region,Region,Total_reads,Total_region_reads,Region_reads,Perc of total reads," # Ref_region used to be Region and current Region used to be amplicon_name
+            "Rpt,Region,Amplicon_name,Total_reads,Total_region_reads,Region_reads,Perc of total reads,"
             "Perc of mapped to region reads,,Total_region_reads MQ>={qual},Region_reads MQ>={qual},"
             "Region_reads 1 MQ>={qual},Region_reads 2 MQ>={qual},Perc Region_reads 1 MQ>={qual},"
             "Perc of mapped to region reads MQ>={qual},,Region_fragments represented MQ>={default_qual},"
